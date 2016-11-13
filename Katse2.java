a1b592364c850b920867bba1ffcdd74080f50e1d
mpalmeos <maie.palmeos@itcollege.ee>
2016-11-13 15:25:13 +0200

    Katse AnimationTimeriga

--- Katse2.java
+++ Katse2.java
@@ -1,8 +1,109 @@
 package TakistusjooksFX;
 
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by Maie on 29/10/2016.
  */
-public class Katse2 {
+public class Katse2 extends Application {
 
+    private ArrayList platvormid = new ArrayList();     // ArrayList, mis hoiab kõik mängulaua elemendid, v.a mängija
+    private HashMap klaviatuur = new HashMap();         // HashMap, mille abil saab hiljem lugeda, missugust nuppu vajutati
+    private Rectangle mangija;                          // Mängija kuubik
+
+    private Pane appRoot = new Pane();                  // appRoot'is toimub kogu liikumine
+    private Pane gameRoot = new Pane();                 //gameRootis' on kõik mängu elemendid (mängija+level)
+
+    private int manguLaius;                             //Teeb kindlaks ühe leveli laiuse
+
+    private boolean hypeVoimalik;
+
+    private double grav;
+
+    private Rectangle tekitaRuut(int x, int y, int w, int h, Color color) {      //Programmisisene meetod elementide loomiseks (mis kõik on ruudud)
+        Rectangle ruut = new Rectangle(w, h);
+        ruut.setTranslateX(x);                              //Seab paika koordinaadid tekkinud ruudule
+        ruut.setTranslateY(y);
+        ruut.setFill(color);
+
+        gameRoot.getChildren().addAll(ruut);                //Tekkinud ruudud pannakse gameRoot'i ehk Pane'i, kus on kõik elemendid
+        return ruut;
+    };
+
+    private void manguSisu() {          //Meetod, mis genereerib terve leveli ette antud andmetest (LevelData).
+        Rectangle taust = new Rectangle(400, 400); //Uus taust (hetkel must ristkülik)
+
+        manguLaius = LevelData.LVL1[0].length()*50; //Mängu laiuseks võta LevelData's oleva stringi ühe "sõna" pikkuse ja korruta 50-ga
+                                                    //(50 on ühe individuaalse ruudu suurus ehk 50x50)
+        for (int i = 0; i < LevelData.LVL1.length; i++) { //Käi läbi iga veerg
+            String rida = LevelData.LVL1[i];              //Salvesta iga veeru andmed
+            for (int j = 0; j < rida.length(); j++) {     //Käi läbi iga rida
+                switch (rida.charAt(j)) {                 //Vastavalt real olevale märgile:
+                    case '0':                             //Kui on "0", siis ära tee midagi (jääb tühjaks)
+                        break;
+                    case'1':                              //Kui on "1", siis tekita sinine ruut (platvorm)
+                        Rectangle platvorm = tekitaRuut(j*50, i*50, 50, 50, Color.BLUE);
+                        platvormid.add(platvorm);         //Lisa tekkinud platvorm ArrayListi platvormid, kusjuures selle
+                        break;                            //asukohaks märgi vastavad i ja j koordinaadid
+                }
+            }
+        }
+        Rectangle mangija = tekitaRuut(30, 30, 30, 30, Color.WHITE);  //Tekita mängija (ruut) ja säti ta teatud koordinaatidele)
+
+        appRoot.getChildren().addAll(taust,gameRoot);     //Pane kogu krempel kõige peamisele Pane'ile ehk sellele, kus toimub liikumine
+    }
+
+    private boolean kasVajutatud(KeyCode key) { //See meetod peaks takistama topelthüpet.
+
+        return (boolean) klaviatuur.getOrDefault(key, false);
+    }
+
+    private void uuenda() {                     //Hüppamist lubav meetod
+
+        if (kasVajutatud(KeyCode.SPACE)) {      //Vastava nupu vajutamisel aktiveerub hüpe.
+            hype();
+        }
+        if (kasVajutatud(KeyCode.W)) {
+            hype();
+        }
+        if (kasVajutatud(KeyCode.UP)){
+            hype();
+        }
+    }
+
+    private void hype() {                       //Hüppamist lubav meetod
+        if (hypeVoimalik) {                     //Kui hypeVoimalik == true, siis vähenda gravitatsiooni.
+            grav = - 10;
+            hypeVoimalik = false;               //Seejärel ütleme ise, et hypeVoimalik == false ehk hype ei tohi enam toimuda.
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        manguSisu();                            //Genereeri mängu sisu ehk lae level.
+
+        Scene esimene = new Scene(appRoot);     //Akna tegemine ja appRoot näitamine
+        primaryStage.setTitle("Takistusjooks");
+        primaryStage.setScene(esimene);
+        primaryStage.show();
+
+        AnimationTimer timer = new AnimationTimer() { //Kogu liikumine, mis mängus toimub.
+            @Override
+            public void handle(long now) {
+                    uuenda();
+            }
+        };timer.start();
+
+    }
 }
